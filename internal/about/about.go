6e800fb8896bb00831db0a9cc640462fabbac60a
Phillarmonkey <phillarmonkey@outlook.com>
2025-04-02 18:53:22 +0100

    Added an about object

---
+++ internal/about/about.go
@@ -0,0 +1,23 @@
+package about
+
+type Datum struct {
+	Name         string   `json:"name"`
+	Version      string   `json:"version"`
+	Description  string   `json:"description,omitempty"`
+	License      string   `json:"license"`
+	Contributors []string `json:"contributors,omitempty"`
+	Homepage     string   `json:"homepage,omitempty"`
+	Repository   string   `json:"repository,omitempty"`
+}
+
+func About() Datum {
+	return Datum{
+		Name:        "SyncopateDB",
+		Description: "A flexible, lightweight data store with advanced query capabilities",
+		Version:     "0.0.1",
+		License:     "MIT",
+		Contributors: []string{
+			"The Phillarmonic Software Team <the PhillarMonkeys>",
+		},
+	}
+}
