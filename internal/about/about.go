903b271dc4be628307173e2b0652dfe8edad9014
Phillarmonkey <phillarmonkey@outlook.com>
2025-05-06 17:45:09 +0100

    Added ignore for usage endpoints. Turned periodic memory log on terminal off by default.

--- internal/about/about.go
+++ internal/about/about.go
@@ -14,7 +14,7 @@ func About() Datum {
 	return Datum{
 		Name:        "SyncopateDB",
 		Description: "A flexible, lightweight data store with advanced query capabilities",
-		Version:     "0.3.0",
+		Version:     "0.4.0",
 		License:     "MIT",
 		Contributors: []string{
 			"The Phillarmonic Software Team <the PhillarMonkeys>",
